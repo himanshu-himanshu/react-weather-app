a4dae934de26744d27a81a4cf86a61e5a6b1b622
Himanshu <himanshu16315@gmail.com>
2022-10-19 16:04:27 -0400

    Popular cities implemented

--- src/components/layout/SearchBar.jsx
+++ src/components/layout/SearchBar.jsx
@@ -15,9 +15,11 @@ const SearchBar = ({ handleInputSearch, handleUnit, unit }) => {
     handleUnit();
   };
 
+  const popularCities = ["Delhi", "Dubai", "Toronto"];
+
   return (
     <>
-      <div className="container max-w-4xl mx-auto text-white flex flex-row justify-evenly sm:justify-between p-2 md:p-4 bg-black opacity-80 rounded-md items-center">
+      <div className="container max-w-4xl mx-auto text-white flex flex-row justify-between sm:justify-between p-2 md:p-4 bg-black opacity-80 rounded-md items-center">
         <div className="flex space-x-2 md:space-x-4 font-Raleway">
           <input
             type="text"
@@ -34,7 +36,21 @@ const SearchBar = ({ handleInputSearch, handleUnit, unit }) => {
             <BsSearch size={28} />
           </button>
         </div>
-        <div className="flex">
+
+        <div className="flex flex-row space-x-12">
+          <div className="hidden md:flex md:items-center flex-row space-x-8 text-gray-200 font-light">
+            {popularCities.map((city, index) => {
+              return (
+                <h1
+                  onClick={() => handleInputSearch(city.toLowerCase())}
+                  className="hover:cursor-pointer hover:text-sky-500"
+                  key={index}
+                >
+                  {city}
+                </h1>
+              );
+            })}
+          </div>
           <button
             className="bg-blue-800 px-6 py-2 text-lg shadow-2xl rounded hover:bg-blue-900 duration-200"
             onClick={() => handleUnitChange()}
